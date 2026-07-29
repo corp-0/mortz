04291cfffa8e8ef0277aae9a0f6ca9d254dffd60
Gilles <43683714+corp-0@users.noreply.github.com>
2026-07-29 12:55:09 -0400

    fix: tests crashing after completion

--- Mortz.Tests/GodotTestCollection.cs
+++ Mortz.Tests/GodotTestCollection.cs
@@ -1,15 +1,24 @@
-using twodog.fixture;
+using twodog.Testing;
 using Xunit;
 
 namespace Mortz.Tests;
 
 public sealed class MortzGodotFixture()
-    : GodotFixtureBase(
+    : FixtureBase(
         "--headless",
         "res://Mortz.Tests/TestRoot.tscn",
         "++",
         "--content-root",
-        Path.Combine(twodog.Engine.ResolveProjectDir(), "content"));
+        Path.Combine(twodog.Engine.ResolveProjectDir(), "content")), IDisposable
+{
+    public new void Dispose()
+    {
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
+        base.Dispose();
+    }
+}
 
 [CollectionDefinition(nameof(MortzGodotCollection), DisableParallelization = true)]
 public sealed class MortzGodotCollection : ICollectionFixture<MortzGodotFixture>;
